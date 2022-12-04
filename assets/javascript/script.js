5b57b2581584f0b595bbb49e2068d648c5d581ae
sdfields <spencerdfields@gmail.com>
2022-12-04 14:15:24 -0600

    added fetch requests for cityurl and weatherurl

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,6 +6,26 @@ var fiveDayForecast = document.getElementById("#5-day-forecast");
 const APIKey = "e9533dd16d43d2d43a7b7da994956940";
 
 function findCity(event) {
-    event.preventDefault()
-    var cityValue = searchCity.value
+    event.preventDefault();
+    var cityValue = searchCity.value;
+    searchCity.value = "";
+    
+    var cityURL = "https://api.openweathermap.org/data/2.5/weather?" + cityValue + "&appid=" + APIKey + "&units=imperial";
+    
+    fetch(cityURL)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data) {
+        let latitude = data.coord.latitude;
+        let longitude = data.coord.longitude;
+        weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey + "&units=imperial";
+    })
+
+    fetch(weatherURL)
+    .then(function(response){
+        return response.json()
+    })
+
 }
+
