7778fc5007e1417b557075578d99d10ba38de423
sdfields <spencerdfields@gmail.com>
2022-12-04 13:29:11 -0600

    added ids to html and variables to js

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+var searchBar = document.getElementById("#search-bar");
+var searchCity = document.getElementById("#search-city");
+var forecast = document.getElementById("#forecast");
+var fiveDayForecast = document.getElementById("#5-day-forecast");
+
+const APIKey = "e9533dd16d43d2d43a7b7da994956940";
+
+function findCity(event) {
+    event.preventDefault()
+    var cityValue = searchCity.value
+}
